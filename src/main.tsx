ab658ba209d1bc0aaadc64ceebb2e5fbb5fc0b19
Kameer Hosein <kameer-hosien@outlook.com>
2024-11-10 19:35:58 -0400

    corrected location of dashboard component

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dasboard";
 import Details from "./pages/Details";
 import Reports from "./pages/Reports";
 import Settings from "./pages/Settings";
